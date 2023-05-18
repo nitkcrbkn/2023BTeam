cff2b1c8652698938d3bb97bcad7b69dc1fdb1ba
starkoka <j22437@inc.kisarazu.ac.jp>
2023-05-18 17:09:06 +0900

    2輪テンプレに変更

--- App/Src/app.c
+++ App/Src/app.c
@@ -3,23 +3,27 @@
 #include "DD_RCDefinition.h"
 #include "SystemTaskManager.h"
 #include <stdlib.h>
+#include "message.h"
 #include "MW_GPIO.h"
 #include "MW_IWDG.h"
-#include "message.h"
 #include "MW_flash.h"
 #include "constManager.h"
-#include <trapezoid_ctrl.h>
-#include "stm32f1xx_hal.h"
-//#include "mbed.h"
-
-static
-int suspensionSystem(int inx, int iny); //足回り
+#include "trapezoid_ctrl.h"
+/*suspensionSystem*/
 
 
 static
-int cont(void);
-//static int LEDSystem(int redledmode,int blueledmode,int greanledmode);
+int suspensionSystem(void);
 
+
+
+/* 腕振り部の変数 */
+int situation = 0;
+int judgepush = 0;
+
+/* モードチェンジ用変数 */
+int mode = 0;
+int checkpush = 0;
 
 /*メモ
  *g_ab_h...ABのハンドラ
@@ -28,12 +32,47 @@ int cont(void);
  *g_rc_data...RCのデータ
  */
 
-int mode=0;
-int flush_ok=1;
+#define WRITE_ADDR (const void*)(0x8000000+0x400*(128-1))/*128[KiB]*/
+flashError_t checkFlashWrite(void){
+  const char data[]="HelloWorld!!TestDatas!!!\n"
+    "however you like this microcomputer, you don`t be kind to this computer.";
+  return MW_flashWrite(data,WRITE_ADDR,sizeof(data));
+}
 
 int appInit(void){
+  message("msg","hell");
+
+  /* switch(checkFlashWrite()){ */
+  /* case MW_FLASH_OK: */
+  /*   message("msg","FLASH WRITE TEST SUCCESS\n%s",(const char*)WRITE_ADDR); */
+  /*   break; */
+  /* case MW_FLASH_LOCK_FAILURE: */
+  /*   message("err","FLASH WRITE TEST LOCK FAILURE\n"); */
+  /*   break; */
+  /* case MW_FLASH_UNLOCK_FAILURE: */
+  /*   message("err","FLASH WRITE TEST UNLOCK FAILURE\n"); */
+  /*   break; */
+  /* case MW_FLASH_ERASE_VERIFY_FAILURE: */
+  /*   message("err","FLASH ERASE VERIFY FAILURE\n"); */
+  /*   break; */
+  /* case MW_FLASH_ERASE_FAILURE: */
+  /*   message("err","FLASH ERASE FAILURE\n"); */
+  /*   break; */
+  /* case MW_FLASH_WRITE_VERIFY_FAILURE: */
+  /*   message("err","FLASH WRITE TEST VERIFY FAILURE\n"); */
+  /*   break; */
+  /* case MW_FLASH_WRITE_FAILURE: */
+  /*   message("err","FLASH WRITE TEST FAILURE\n"); */
+  /*   break;         */
+  /* default: */
+  /*   message("err","FLASH WRITE TEST UNKNOWN FAILURE\n"); */
+  /*   break; */
+  /* } */
+  /* flush(); */
 
   ad_init();
+
+  message("msg","plz confirm\n%d\n",g_adjust.rightadjust.value);
 
   /*GPIO の設定などでMW,GPIOではHALを叩く*/
   return EXIT_SUCCESS;
@@ -42,325 +81,101 @@ int appInit(void){
 /*application tasks*/
 int appTask(void){
   int ret=0;
+
+  /*if(__RC_ISPRESSED_R1(g_rc_data)&&__RC_ISPRESSED_R2(g_rc_data)&&
+     __RC_ISPRESSED_L1(g_rc_data)&&__RC_ISPRESSED_L2(g_rc_data)){
+    while(__RC_ISPRESSED_R1(g_rc_data)||__RC_ISPRESSED_R2(g_rc_data)||
+	  __RC_ISPRESSED_L1(g_rc_data)||__RC_ISPRESSED_L2(g_rc_data));
+    ad_main();
+    }*/
+	 
   /*それぞれの機構ごとに処理をする*/
   /*途中必ず定数回で終了すること。*/
-  
-  ret = cont();
+  ret = suspensionSystem();
   if(ret){
     return ret;
   }
 
-    return EXIT_SUCCESS;
-}
-
-static int cont(void){
-
-  //static int (*towfcp[])(int) = {ThrowSystem_1,ThrowSystem_2,ThrowSystem_3,ThrowSystem_4};
-  //static int (*shefcp[])(int) = {ThrowSystem_5};//関数ポインタ定義、使う関数の指定を関数ポインタで　まだ未完成
-  int ret;
-  //int i = 0;
-  static int screencheck1=0,screencheck2=0,screencheck3=0;
-  //static int reloadcheck1=0,reloadcheck2=0,reloadcheck3=0;
-  static int towelmode=-1,sheetmode=-1;
-  //static int towelpress=0,towelcheck=0,towelgo=0;
-  //static int sheetpress=0,sheetcheck=0,sheetgo=0;
-  static int beformode=0;
-  //static int towelstatus=1;
-  //static int sheetstatus=1;
-  //  static int color1=0,color2=0,color3=0,color4=0;
-  static int count=0;
-  static int inx=0;
-  static int iny=0;
-  static int redledmode=0,blueledmode=0,greanledmode=0;
-  //画面
-  if((__RC_ISPRESSED_R2(g_rc_data))&&(__RC_ISPRESSED_L2(g_rc_data))){
-    if(__RC_ISPRESSED_UP(g_rc_data))screencheck1=1;
-    if(__RC_ISPRESSED_RIGHT(g_rc_data)&&screencheck1)screencheck2=1;
-    if(__RC_ISPRESSED_DOWN(g_rc_data)&&screencheck2)screencheck3=1;
-    if(__RC_ISPRESSED_LEFT(g_rc_data)&&screencheck3){
-      mode^=1;screencheck1=0;screencheck2=0;screencheck3=0;
-    }
-  }else{
-    screencheck1=0;screencheck2=0;screencheck3=0;
-  }
-
-  if(__RC_ISPRESSED_R1(g_rc_data)&&__RC_ISPRESSED_L1(g_rc_data)){
-    redledmode=1;
-  }else{
-    redledmode=0;
-  }
-
-  /*ret = LEDSystem(redledmode,blueledmode,greanledmode);
-  if(ret){
-    return ret;
-  }*/
-
-  ret = suspensionSystem(inx,iny);
-  if(ret){
-    return ret;
-  }
-
-  
-
-  
-  count++;
-  if(flush_ok){
-    if(!mode){
-      MW_printf("\ntmode[%2d] smode[%2d] NUM_MD[%d] RLED[%d] BLED[%d] GLED[%d]",towelmode,sheetmode,DD_NUM_OF_MD,redledmode,blueledmode,greanledmode);
-    }else{
-      MW_printf(/*左スティックで移動、右スティックで旋回\n丸でタオル砲、三角でシーツ砲\nR1、L1同時押ししながら↑ → ↓ ← の順に押すと各砲をリセット\n*/"MD\n0番：左手前\n1番：左奥\n2番：右奥\n3番：右手前");
-    }
-    flush_ok=0;
-  }
-
-  if(beformode!=mode){
-    MW_printf("\33[2J");
-    beformode=mode;
-  }
+	 
   return EXIT_SUCCESS;
 }
 
+
+/*プライベート 足回りシステム*/
 static
-int suspensionSystem(int inx,int iny){
-  const int num_of_motor = 4;/*モータの個数*/
-  //  int rc_analogdata;/*アナログデータ*/
-  unsigned int idx=0;/*インデックス*/
-  int i=0,x=0,y=0,w=0;
-  int m=0;
-  int adjust;
-  // static int x_ok=0,y_ok=0;
-  //static int xflag=0,yflag=0;
- const tc_const_t tc ={
-    .inc_con = 400,
-    .dec_con = 450,
- };
+int suspensionSystem(void){
+  const int num_of_motor = 2;/*モータの個数*/
+  int rc_analogdata;/*アナログデータ*/
+  unsigned int idx;/*インデックス*/
+  int i;
 
- 
- if(iny){
-   x = iny;
- }else{
-   x = DD_RCGetLY(g_rc_data);
- }
+  const tc_const_t tc ={
+    .inc_con = 100,
+    .dec_con = 225,
+  };
 
- if(inx){
-   y = inx;
- }else{
-    y = DD_RCGetLX(g_rc_data);
- }
-
- /*
- if(iny){
-   
-   if(iny>0||yflag){
-     if(x<0&&i<10){
-       yflag=1;
-       i++;
-     }else{
-       x_ok=1;
-     }
-   }else if(iny<0||yflag){
-     if(x>0&&i<10){
-       yflag=1;
-       i++;
-     }else{
-       x_ok=1;
-     }
-   }else{
-     x_ok=1;
-   }
-   if(x_ok){
-     x = iny;
-     i=0;
-     yflag=0;
-   }
- }else{
-   
-   if(DD_RCGetLY(g_rc_data)>0||yflag){
-     if(x<0&&i<10){
-       yflag=1;
-       i++;
-     }else{
-       x_ok=1;
-       i=0;
-     }
-   }
-   if(DD_RCGetLY(g_rc_data)<0){
-     if(x>0&&i<10){
-       i++;
-     }else{
-       x_ok=1;
-       i=0;
-     }
-   }
-   if(x_ok){
-     x = DD_RCGetLY(g_rc_data);
-   }
- }
-
-  if(inx){
-
-   if(inx>0){
-     if(y<0&&i<10){
-       i++;
-     }else{
-       y_ok=1;
-       i=0;
-     }
-   }else if(inx<0){
-     if(y>0&&i<10){
-       i++;
-     }else{
-       y_ok=1;
-       i=0;
-     }
-   }
-   if(y_ok){
-     y = inx;
-   }
- }else{
-   if(DD_RCGetLX(g_rc_data)>0){
-     if(y<0&&i<10){
-       i++;
-     }else{
-       y_ok=1;
-       i=0;
-     }
-   }
-   if(DD_RCGetLX(g_rc_data)<0){
-     if(y>0&&i<10){
-       i++;
-     }else{
-       y_ok=1;
-       i=0;
-     }
-   }
-   if(y_ok){
-     y = DD_RCGetLX(g_rc_data);
-   }
- }
- */
- 
- w = -(int)(DD_RCGetRX(g_rc_data)*WISE_MAG);
- /*
- if(x>0){
-   w - 9;
- }
- */ /* if(__RC_ISPRESSED_R2(g_rc_data)){
-     w = 60;
-     }else if(__RC_ISPRESSED_L2(g_rc_data)){
-     w = -60;
-     } else {
-     w = 0;
-     }*/
-
-
-  /*for each motor*/
-  for(i=0;i<num_of_motor;i++){
-    /*それぞれの差分*/
-    switch(i){//トランザム
-    case 0:
-      idx = MECHA1_MD0; //駆動左前用モータ
-      m =  x + y + w;
-      m *= (int)(75*MD0MAG);
-      if(abs(m) <= 3200) {//dutyが低かったら引き上げ
-	m *= 2;
-      } else if(abs(m) >= 6500) {//dutyが6500を超えたら6500以下になるよう調整
-	adjust = abs(m) - 6500;
-	if(m > 0) {
-	  m -= adjust;
-	} else if(m < 0) {
-	  m += adjust;
-	}
-      }
-      break;
-      
-    case 1:
-      idx = MECHA1_MD1; //駆動左後用モータ
-      m =  x - y + w;
-      m *= 75;
-      if(abs(m) <= 3200) {//dutyが低かったら引き上げ
-	m *= 2;
-      } else if(abs(m) >= 6500) {//dutyが6500を超えたら6500以下になるよう調整
-	adjust = abs(m) - 6500;
-	if(m > 0) {
-	  m -= adjust;
-	} else if(m < 0) {
-	  m += adjust;
-	}
-      }
-      break;
-      
-    case 2:
-      idx = MECHA1_MD2; //駆動右後用モータ
-      m = - x - y + w;
-      m *= (int)(75*MD2MAG);
-      if(abs(m) <= 3200) {//dutyが低かったら引き上げ
-	m *= 2;
-      } else if(abs(m) >= 6500) {//dutyが6500を超えたら6500以下になるよう調整
-	adjust = abs(m) - 6500;
-	if(m > 0) {
-	  m -= adjust;
-	} else if(m < 0) {
-	  m += adjust;
-	}
-      }
-      break;
-      
-    case 3:
-      idx = MECHA1_MD3; //駆動右前用モータ
-      m = - x + y + w;
-      m *= 75;
-      if(abs(m) <= 3200) {//dutyが低かったら引き上げ
-	m *= 2;
-      } else if(abs(m) >= 6500) {//dutyが6500を超えたら6500以下になるよう調整
-	adjust = abs(m) - 6500;
-	if(m > 0) {
-	  m -= adjust;
-	} else if(m < 0) {
-	  m += adjust;
-	}
-      }
-      break;
-      
-      return EXIT_FAILURE;
+  /* コントローラのボタンは押されてるか */
+  if(!__RC_ISPRESSED_TRIANGLE(g_rc_data)){
+    checkpush = 1;
     }
-    trapezoidCtrl((int)(SUSPENSION_MAG*m),&g_md_h[idx],&tc);
-  }
-
-  return EXIT_SUCCESS;
-}
-
-
-/*static int LEDSystem(int redledmode,int blueledmode,int greanledmode){
-  switch(redledmode){
-  case 0:
-    MW_GPIOWrite(GPIOBID,GPIO_PIN_13, GPIO_PIN_RESET);
-    break;
-  case 1:
-    MW_GPIOWrite(GPIOBID,GPIO_PIN_13, GPIO_PIN_SET);
-    break;
-  case 2:
-    MW_GPIOWrite(GPIOBID,GPIO_PIN_13, GPIO_PIN_SET);
-    break;
-  }
   
-  switch(blueledmode){
-  case 0:
-    MW_GPIOWrite(GPIOBID,GPIO_PIN_14, GPIO_PIN_RESET);
-    break;
-  case 1:
-    MW_GPIOWrite(GPIOBID,GPIO_PIN_14, GPIO_PIN_SET);
-    break;
+
+  if(__RC_ISPRESSED_TRIANGLE(g_rc_data) && checkpush == 1 && !__RC_ISPRESSED_CROSS(g_rc_data)){
+	    
+    switch(mode){
+    case 1:
+      mode = 0;
+      break;
+    case 0:
+      mode = 1;
+      break;
+    }
+	    
+    checkpush = 0;
   }
 
-  switch(greanledmode){
+  switch(mode){
   case 0:
-    MW_GPIOWrite(GPIOBID,GPIO_PIN_15, GPIO_PIN_RESET);
+    /*for each motor*/
+    for(i=0;i<num_of_motor;i++){
+      /*それぞれの差分*/
+      switch(i){
+      case 0:
+	idx = MECHA1_MD0;
+	rc_analogdata = DD_RCGetRY(g_rc_data);
+	break;
+      case 1:
+	idx = MECHA1_MD1;
+	rc_analogdata = -DD_RCGetLY(g_rc_data);
+	break;     
+	     
+      default:
+	return EXIT_FAILURE;
+      }
+      trapezoidCtrl(rc_analogdata * MD_GAIN,&g_md_h[idx],&tc);
+    }
     break;
-  case 1:
-    MW_GPIOWrite(GPIOBID,GPIO_PIN_15, GPIO_PIN_SET);
-    break;
+
+  /*case 1:
+    for(i=0;i<num_of_motor;i++){
+      switch(i){
+      case 0:
+	idx = MECHA1_MD1;
+	rc_analogdata = DD_RCGetRY(g_rc_data);
+	break;
+      case 1:
+	idx = MECHA1_MD0;
+	rc_analogdata = -DD_RCGetLY(g_rc_data);
+	break;     
+	    
+      default:
+	return EXIT_FAILURE;
+      }
+      trapezoidCtrl(rc_analogdata * MD_GAIN,&g_md_h[idx],&tc);
+    }
+    break;*/
   }
   return EXIT_SUCCESS;
 }
-*/
+
+
--- App/Src/deviceDefinition.c
+++ App/Src/deviceDefinition.c
@@ -13,7 +13,7 @@ DD_MDHand_t g_md_h[DD_NUM_OF_MD] = {
   { .add = 0x11, /* address */
     .duty = 0, /* default duty */
     .mode = D_MMOD_FREE, /* mode */
-  },
+  }/*,
   { .add = 0x12, 
       .duty = 0, // default duty 
     .mode = D_MMOD_FREE, // mode 
@@ -22,7 +22,7 @@ DD_MDHand_t g_md_h[DD_NUM_OF_MD] = {
     .duty = 0, // default duty
     .mode = D_MMOD_FREE, // mode 
   },
-  /*  { .add = 0x14,
+    { .add = 0x14,
     .duty = 0,
     .mode = D_MMOD_FREE,
   },
