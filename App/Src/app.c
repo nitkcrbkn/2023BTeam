273698be4bc4b961a2f372759866deaca20ac512
starkoka <j22437@inc.kisarazu.ac.jp>
2023-08-12 16:00:50 +0900

    先ほどの修正で防ぎきれない部分を修正

--- App/Src/app.c
+++ App/Src/app.c
@@ -169,10 +169,10 @@ int suspensionSystem(void){
                     return EXIT_FAILURE;
             }
             for(int j=0;j<=2;j+=2){ //2つのタイヤを回転させるためにfor文
-                if(j==2 && i==0 && DD_RCGetRY(g_rc_data)==0){
+                if(j==2 && i==0 && __RC_ISPRESSED_L2(g_rc_data)==0){
                     break;
                 }
-                else if(j==2  && i==1 && DD_RCGetLY(g_rc_data)==0){
+                else if(j==2  && i==1 && __RC_ISPRESSED_R2(g_rc_data)==0){
                     break;
                 }
                 trapezoidCtrl(rc_analogdata * MD_GAIN,&g_md_h[idx+j],&tc);
