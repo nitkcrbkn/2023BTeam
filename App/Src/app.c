5e51d16f0d6efd13f9fd1143266e5617d1163da0
starkoka <j22437@inc.kisarazu.ac.jp>
2023-09-21 02:26:46 +0900

    誤差調整

--- App/Src/app.c
+++ App/Src/app.c
@@ -42,7 +42,7 @@ int checkpush = 0;
 #define WRITE_ADDR (const void*)(0x8000000+0x400*(128-1))/*128[KiB]*/
 flashError_t checkFlashWrite(void){
     const char data[]="HelloWorld!!TestDatas!!!\n"
-        "however you like this microcomputer, you don`t be kind to this computer.";
+                      "however you like this microcomputer, you don`t be kind to this computer.";
     return MW_flashWrite(data,WRITE_ADDR,sizeof(data));
 }
 
@@ -95,7 +95,7 @@ int appTask(void){
 	    __RC_ISPRESSED_L1(g_rc_data)||__RC_ISPRESSED_L2(g_rc_data));
         ad_main();
         }*/
-	 
+
     /*それぞれの機構ごとに処理をする*/
     /*途中必ず定数回で終了すること。*/
     ret = suspensionSystem();
@@ -103,20 +103,20 @@ int appTask(void){
         return ret;
     }
 
-  ret = forwardWheelLeft();
-  if(ret){
-      return ret;
-  }
-  ret = OtasukeUpDownSystem();
-  if(ret){
-    return ret;
-  }
+    ret = forwardWheelLeft();
+    if(ret){
+        return ret;
+    }
+    ret = OtasukeUpDownSystem();
+    if(ret){
+        return ret;
+    }
 
     ret = forwardWheelRight();
     if(ret){
         return ret;
     }
-	 
+
     return EXIT_SUCCESS;
 }
 
@@ -128,99 +128,100 @@ int suspensionSystem(void){
     int rc_analogdata;/*アナログデータ*/
     unsigned int idx;/*インデックス*/
     int i;
+    int dutyDifference[2][3]={{100,0,100},{109,0,103}};
 
     const tc_const_t tc ={
-        .inc_con = 100,
-        .dec_con = 225,
+            .inc_con = 100,
+            .dec_con = 225,
     };
 
     /* コントローラのボタンは押されてるか */
     if(!__RC_ISPRESSED_TRIANGLE(g_rc_data)){
         checkpush = 1;
     }
-    
+
 
     if(__RC_ISPRESSED_TRIANGLE(g_rc_data) && checkpush == 1 && !__RC_ISPRESSED_CROSS(g_rc_data)){
-	        
+
         switch(mode){
-        case 1:
-            mode = 0;
-            break;
-        case 0:
-            mode = 1;
-            break;
+            case 1:
+                mode = 0;
+                break;
+            case 0:
+                mode = 1;
+                break;
         }
-	        
+
         checkpush = 0;
     }
 
     switch(mode){
-    case 0:
-        /*for each motor*/
-        for(i=0;i<num_of_motor;i++){
-            /*それぞれの差分*/
-            switch(i){
-                case 0:
-	                idx = MECHA1_MD0;
-	                rc_analogdata = DD_RCGetRY(g_rc_data);
-	                break;
+        case 0:
+            /*for each motor*/
+            for(i=0;i<num_of_motor;i++){
+                /*それぞれの差分*/
+                switch(i){
+                    case 0:
+                        idx = MECHA1_MD0;
+                        rc_analogdata = DD_RCGetRY(g_rc_data);
+                        break;
 
-                case 1:
-	                idx = MECHA1_MD1;
-	                rc_analogdata = -DD_RCGetLY(g_rc_data);
-                    break;
-                
-                default:
-                    return EXIT_FAILURE;
-            }
+                    case 1:
+                        idx = MECHA1_MD1;
+                        rc_analogdata = -DD_RCGetLY(g_rc_data);
+                        break;
 
-            int dutyX;
-
-            if(__RC_ISPRESSED_L1(g_rc_data) && __RC_ISPRESSED_R1(g_rc_data)){
-                dutyX = 4;
-            }
-            else if(__RC_ISPRESSED_L1(g_rc_data) || __RC_ISPRESSED_R1(g_rc_data)){
-                dutyX = 2;
-            }
-            else{
-                dutyX = 1;
-            }
-
-            for(int j=0;j<=2;j+=2){ //2つのタイヤを回転させるためにfor文
-                if(j==2 && i==0 && __RC_ISPRESSED_R1(g_rc_data) && DD_RCGetRY(g_rc_data)==0){
-                    break;
+                    default:
+                        return EXIT_FAILURE;
                 }
-                else if(j==2  && i==1 && __RC_ISPRESSED_L1(g_rc_data) && DD_RCGetLY(g_rc_data)==0){
-                    break;
+
+                int dutyX;
+
+                if(__RC_ISPRESSED_L1(g_rc_data) && __RC_ISPRESSED_R1(g_rc_data)){
+                    dutyX = 4;
                 }
-                if(DD_RCGetRY(g_rc_data)-DD_RCGetLY(g_rc_data) >= -10 && DD_RCGetRY(g_rc_data)-DD_RCGetLY(g_rc_data) <= 10){
-                    trapezoidCtrl(rc_analogdata * MD_GAIN_MAX / dutyX,&g_md_h[idx+j],&tc);
+                else if(__RC_ISPRESSED_L1(g_rc_data) || __RC_ISPRESSED_R1(g_rc_data)){
+                    dutyX = 2;
                 }
                 else{
-                    trapezoidCtrl(rc_analogdata * MD_GAIN_NORMAL / dutyX,&g_md_h[idx+j],&tc);
+                    dutyX = 1;
+                }
+
+                for(int j=0;j<=2;j+=2){ //2つのタイヤを回転させるためにfor文
+                    if(j==2 && i==0 && __RC_ISPRESSED_R1(g_rc_data) && DD_RCGetRY(g_rc_data)==0){
+                        break;
+                    }
+                    else if(j==2  && i==1 && __RC_ISPRESSED_L1(g_rc_data) && DD_RCGetLY(g_rc_data)==0){
+                        break;
+                    }
+                    if(DD_RCGetRY(g_rc_data)-DD_RCGetLY(g_rc_data) >= -10 && DD_RCGetRY(g_rc_data)-DD_RCGetLY(g_rc_data) <= 10){
+                        trapezoidCtrl(rc_analogdata * MD_GAIN_MAX / dutyX / dutyDifference[i][j] * 100,&g_md_h[idx+j],&tc);
+                    }
+                    else{
+                        trapezoidCtrl(rc_analogdata * MD_GAIN_NORMAL / dutyX / dutyDifference[i][j] * 100,&g_md_h[idx+j],&tc);
+                    }
                 }
             }
-        }
-        break;
+            break;
 
-    /*case 1:
-        for(i=0;i<num_of_motor;i++){
-            switch(i){
-            case 0:
-	idx = MECHA1_MD1;
-	rc_analogdata = DD_RCGetRY(g_rc_data);
-	break;
-            case 1:
-	idx = MECHA1_MD0;
-	rc_analogdata = -DD_RCGetLY(g_rc_data);
-	break;         
-	        
-            default:
-	return EXIT_FAILURE;
-            }
-            trapezoidCtrl(rc_analogdata * MD_GAIN,&g_md_h[idx],&tc);
-        }
-        break;*/
+            /*case 1:
+                for(i=0;i<num_of_motor;i++){
+                    switch(i){
+                    case 0:
+            idx = MECHA1_MD1;
+            rc_analogdata = DD_RCGetRY(g_rc_data);
+            break;
+                    case 1:
+            idx = MECHA1_MD0;
+            rc_analogdata = -DD_RCGetLY(g_rc_data);
+            break;
+
+                    default:
+            return EXIT_FAILURE;
+                    }
+                    trapezoidCtrl(rc_analogdata * MD_GAIN,&g_md_h[idx],&tc);
+                }
+                break;*/
     }
     return EXIT_SUCCESS;
 }
@@ -250,25 +251,25 @@ int forwardWheelLeft(void){
 
 static
 int OtasukeUpDownSystem(void){
-  unsigned int idx = 4;
-  int i;
-  int duty = 0;
+    unsigned int idx = 4;
+    int i;
+    int duty = 0;
 
-  const tc_const_t tc ={
-    .inc_con = 100,
-    .dec_con = 225,
-  };
-  if(__RC_ISPRESSED_UP(g_rc_data)){
-      duty = -6000;
-  } else if(__RC_ISPRESSED_DOWN(g_rc_data)){
-      duty = 6000;
-  } else{
-      duty = 0;
-  }
+    const tc_const_t tc ={
+            .inc_con = 100,
+            .dec_con = 225,
+    };
+    if(__RC_ISPRESSED_UP(g_rc_data)){
+        duty = -6000;
+    } else if(__RC_ISPRESSED_DOWN(g_rc_data)){
+        duty = 6000;
+    } else{
+        duty = 0;
+    }
 
-  trapezoidCtrl(duty,&g_md_h[idx],&tc);
+    trapezoidCtrl(duty,&g_md_h[idx],&tc);
 
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
 static
