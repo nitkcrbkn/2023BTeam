f80b20cf4db3bc413fb51a8ad9c9b6f220c40afa
starkoka <j22437@inc.kisarazu.ac.jp>
2023-09-21 13:42:27 +0900

    MDを6枚化

--- App/Src/deviceDefinition.c
+++ App/Src/deviceDefinition.c
@@ -17,12 +17,16 @@ DD_MDHand_t g_md_h[DD_NUM_OF_MD] = {
   { .add = 0x12, 
       .duty = 0, // default duty 
     .mode = D_MMOD_FREE, // mode 
-  },/*
+  },
   { .add = 0x13,
     .duty = 0, // default duty
     .mode = D_MMOD_FREE, // mode 
   },
     { .add = 0x14,
+    .duty = 0,
+    .mode = D_MMOD_FREE,
+  },
+    { .add = 0x15,
     .duty = 0,
     .mode = D_MMOD_FREE,
   },
